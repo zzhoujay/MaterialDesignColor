6da14453febfdd47b20a88e547aa92adfa26a73a
zzhoujay <1582519486@qq.com>
2016-03-08 23:15:34 +0800

    将项目发布到了jcenter

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-#MaterialDesignColor
+# MaterialDesignColor
 
 > 谷歌官方的MaterialDesign颜色代码
+
+### 使用
+
+```compile 'com.zzhoujay.materialcolors:materialcolors:1.0.0'```
+
+在代码中使用诸如```R.color.material_blue_500```来引用对应的颜色值，其中`material`为前缀，`blue`为颜色类别，`500`为颜色深度
+
+具体数值参照如下图片：
+
 
 ![Color](http://git.oschina.net/uploads/images/2015/0612/150439_5de9e317_141009.png "Material Design Color")
 
---
+++ app/src/main/java/com/zzhoujay/materialcolor/MainActivity.java
@@ -0,0 +1,17 @@
+package com.zzhoujay.materialcolor;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+/**
+ * Created by zhou on 16-3-8.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
